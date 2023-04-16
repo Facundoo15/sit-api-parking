0ff40b74dd07b1c54a71b7fefe73f72e6d60a1ee
Favio Angulo <favii_01@hotmail.com>
2023-04-16 12:14:48 -0500

    feat: Crea la entidad Car

--- api-parking/api-parking/src/main/java/com/parking/apiparking/entities/Car.java
+++ api-parking/api-parking/src/main/java/com/parking/apiparking/entities/Car.java
@@ -1,4 +1,13 @@
 package com.parking.apiparking.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Car {
+    private String licensePlate;
+    private String color;
+    private String LocalDateTime;
+
 }
