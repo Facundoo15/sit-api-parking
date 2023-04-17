e5fb546264f97bc717dbf918e54e4d797b5ee3d7
Favio Angulo <favii_01@hotmail.com>
2023-04-16 23:25:19 -0500

    feat: Se agregó la funcionalidad de calcular el precio del parkeo de cada uno y correción de algunos detalles en los métodos

--- api-parking/api-parking/src/main/java/com/parking/apiparking/service/ParkingService.java
+++ api-parking/api-parking/src/main/java/com/parking/apiparking/service/ParkingService.java
@@ -4,6 +4,8 @@ import com.parking.apiparking.entities.Car;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -26,33 +28,47 @@ public class ParkingService {
     }
 
     // TODO: US2 Como usuario, quiero poder buscar un auto por su placa, para poder encontra rápidamente un vehículo en particular.
-    public Optional<Car> getCarByLicense(String license){
-        return parkingLot.stream().filter(e ->  e.getLicensePlate().equalsIgnoreCase(license)).findFirst();
+    public Optional<Car> getCarByLicense(String license) {
+        return parkingLot.stream().filter(e -> e.getLicensePlate().equalsIgnoreCase(license)).findFirst();
     }
+
     // TODO: US3 Como usuario, quiero poder buscar autos por su color, para poder encontrar todos los vehículos de un mismo color en el estacionamiento.
-    public List<Car> getCarsByLicense(String color){
+    public List<Car> getCarsByColor(String color) {
         return parkingLot.stream().filter(e -> e.getColor().equals(color)).toList();
     }
 
     // TODO: US4 Como usuario, quiero poder agregar un auto al parqueo, para poder estacionar mi vehículo.
-    public void addCar(Car car){
+    public void addCar(Car car) {
         this.parkingLot.add(car);
     }
 
 
-
     // Método para eliminar un auto por la licencia
-    public boolean removeCarbyLicense(String license){
+    public boolean removeCarbyLicense(String license) {
         return this.parkingLot.removeIf(e -> e.getLicensePlate().equalsIgnoreCase(license));
     }
 
     // Método para parkear un auto y asignarle su hora de ingreso
-    public void parkCar(Car car){
+    public void parkCar(Car car) {
         car.setEntryTime(LocalDateTime.now());
         addCar(car);
     }
 
-    
+    // TODO: US5 Como usuario, quiero poder sacar un auto del parqueo y calcular la tarifa de
+    // estacionamiento, para poder salir del estacionamiento y pagar la tarifa
+    // correspondiente
 
+    public double calculateParking(String license){
+        Optional<Car> optionalCar = getCarByLicense(license);
+        if(optionalCar.isPresent()){
+            Car car = optionalCar.get();
+            LocalDateTime timePark = car.getEntryTime();
+            if(timePark != null){
+                long timeHoursParked = ChronoUnit.HOURS.between(timePark, LocalDateTime.now());
+                return timeHoursParked * 25;
+            }
+        }
+        return 0;
+    }
 
 }
